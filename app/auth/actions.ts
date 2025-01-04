8f5eaec80905b1db3e589d00578b6d540845a119
Zen T <ztang230@gmail.com>
2025-01-04 15:18:53 -0600

    hm add some auth

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -6,16 +6,19 @@ import { cookies } from 'next/headers'
 export async function signIn() {
   const cookieStore = cookies()
   const supabase = await createClient(cookieStore)
-  await supabase.auth.signInWithOAuth({
+  const res = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
       redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
     },
   })
+  console.log('Sign in result:', res)
 }
 
 export async function signOut() {
   const cookieStore = cookies()
   const supabase = await createClient(cookieStore)
-  await supabase.auth.signOut()
+  const res = await supabase.auth.signOut()
+  console.log('Sign out result:', res)
+  
 }
